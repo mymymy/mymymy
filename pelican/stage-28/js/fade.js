725b1e66318635c85f9ffb22ab1cbc8021a34df1
Matthew Young <matt@mymymy.co.uk>
2017-02-18 20:34:15 +0000

    Wipe the slate clean. Remove everything.

--- pelican/stage-28/js/fade.js
+++
@@ -1,7 +0,0 @@
-$(".logo").delay(1000).animate({"opacity": "1"}, 1000);
-
-$("#a-pelican-introduction").delay(1000).animate({"opacity": "1"}, 1000);
-
-$("h1").delay(2000).animate({"opacity": "1"}, 1000);
-
-$("h2").delay(3000).animate({"opacity": "0.4"}, 1000);
--- pelican/stage-29/js/fade.js
+++
@@ -1,3 +0,0 @@
-$("#cover").delay(1000).animate({"opacity": "1"}, 2000);
-
-$("#title-block").delay(2000).animate({"opacity": "1"}, 1000);
