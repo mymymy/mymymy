5777de6247868bbab94c345383b2e9822419247c
Matthew Young <matt@mymymy.co.uk>
2017-04-15 23:25:46 +0100

    Scrap previous stuff. New focus on Dawkins page. Add basic layout and intro.

--- gruntfile.js
+++ gruntfile.js
@@ -76,7 +76,7 @@ module.exports = function(grunt) {
 				files: [
 					{ expand: true, flatten: true, src: ['dist/assets/app.js'], dest: 'dist/assets/' },
 					{ expand: true, flatten: true, src: ['dist/assets/main.css'], dest: 'dist/assets/' },
-					{ expand: true, flatten: true, cwd: "dist", src: ['*.html'], dest: 'dist/' }
+					{ expand: true, flatten: true, cwd: "dist", src: ['*'], dest: 'dist/' }
 				]
 			},
 			local: {
@@ -89,7 +89,7 @@ module.exports = function(grunt) {
 				files: [
 					{ expand: true, flatten: true, src: ['dist/assets/app.js'], dest: 'dist/assets/' },
 					{ expand: true, flatten: true, src: ['dist/assets/main.css'], dest: 'dist/assets/' },
-					{ expand: true, flatten: true, cwd: "dist", src: ['*.html'], dest: 'dist/' }
+					{ expand: true, flatten: true, cwd: "dist", src: ['*'], dest: 'dist/' }
 				]
 			}
 		},
